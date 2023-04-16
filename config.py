e365605e7533ac8a2e7dbb5ca1142790f88c0539
janfiszer <jf.janfiszer@gmail.com>
2023-04-16 11:39:08 +0200

    model eval: probabilities shown, futher tests

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 # word2vec parameters
 VECTOR_SIZE = 200
 WINDOW = 7
-MIN_COUNT = 50
+MIN_COUNT = 20
 # unusual parameter
 # determine how many words will be used to predict the next one
 PREVIOUS_WORDS_CONSIDERED  = 3
 END_TOKEN = '[END]'
 
+# for the NN
+INPUT_SIZE = PREVIOUS_WORDS_CONSIDERED * VECTOR_SIZE
+
 # training parameters
 EPOCHS = 64
 BATCH_SIZE = 128
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-3
 LABEL_SMOOTHING_RATIO = 0.2
