67a5a30001e6a5315b681ce11edd4f7cf4f27f9c
janfiszer <jf.janfiszer@gmail.com>
2023-04-28 18:48:19 +0200

    smarter vectorizing- more efficient, lets to create dataset for RNNs

--- utils/DataGenerator.py
+++ utils/DataGenerator.py
@@ -1,8 +1,9 @@
 import numpy as np
-from typing import Iterable
+from typing import Iterable, Tuple
 from collections import Counter
 
 from utils.exceptions.NotInVocabularyException import NotInVocabularyException
+
 
 class DataGenerator():
     def __init__(self, tokenized_documents: Iterable, vocabulary=None):
@@ -31,8 +32,7 @@ class DataGenerator():
         vocabulary.extend(extra_tokens)
 
         self.vocabulary = vocabulary
-    
-    
+
     def create_dataset(self, previous_words_considered=3):
         """ 
         Returns tuple with two lists. First words (previous_words_considered determine how many) and in the second the following word
@@ -53,30 +53,69 @@ class DataGenerator():
                         X_words.append(tokenized_review[index-previous_words_considered: index])
                         y_words.append(tokenized_review[index])
 
+        return X_words, y_words
 
-        return (X_words, y_words)
-    
-
-    def vectorize(self, X_words, word_vectorizer, input_size):
+    def vectorize(self, X_words, word_vectorizer, input_size: Tuple):
         """ 
         Converts a 2D list of tokens into a 2D array of size where each row is a concatenation of
         given tokens, described by input_size = previous_words_considered * vector_size (see config.py).
         """
-        X = np.zeros((len(X_words), input_size))
+        try:
+            input_shape = (len(X_words), )
+            input_shape += input_size
+        except TypeError:
+            print(F"Expected a tuple as input_size, got {type(input_size)}")
+            return
+
+        X = np.zeros(input_shape)
+
+        if len(input_shape) == 3:
+            concatenate_axis = 0
+        elif len(input_shape) == 2:
+            concatenate_axis = 1
+        # TODO: Exception?
+        else:
+            return None
+
+        for index, words in enumerate(X_words):
+            # initialization the numpy array with the embedding of the first word
+            # idea!! first iterate over the word and create their embeddings
+            # then np.concatenate them with appropiate axis
+            embedding_list = [[word_vectorizer.wv.get_vector(word)] for word in words]
+            embedded_np_array = np.concatenate(embedding_list, axis=concatenate_axis)
+
+
+            # numpy_x = word_vectorizer.wv.get_vector(words[0])
+            # for i in range(1, len(words)):
+            #     # the mistake is here it hase to be with the vector dim , still not working should be debugged
+            #     try:
+            #         embedded_word = word_vectorizer.wv.get_vector(words[i])
+            #         numpy_x = np.concatenate((numpy_x, embedded_word), axis=concatenate_axis)
+            #     except KeyError:
+            #         raise NotInVocabularyException
+
+            X[index, ...] = embedded_np_array
+
+        return X
+
+    # TODO: rename
+    def vectorize_lstm(self, X_words, word_vectorizer, vector_size, previous_word_considered):
+
+        X = np.zeros((len(X_words), vector_size, previous_word_considered))
 
         for index, x in enumerate(X_words):
             numpy_x = np.array([])
             for word in x:
                 try:
                     numpy_x = np.append(numpy_x, word_vectorizer.wv.get_vector(word))
-                    
+
                 except KeyError:
                     raise NotInVocabularyException
 
-            X[index, ...]  = numpy_x
+            X[index, ...] = numpy_x
 
         return X
-            
+
     def belong_to_vocabulary(self, tokens: list):
         """ 
         Returns the first not belonging token in the vocabulary
